5a6b04f98d7377d696b6aa0f13cce1c682f2e9d0
Aji Nuansa Sengarie <ansengarie@gmail.com>
2023-09-20 10:38:21 +0700

    added set session

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,5 +1,20 @@
+// authenticated.js
+
+let timer
+
 export default function ({ route, redirect }) {
-  if (!localStorage.getItem('token') && route.path !== '/auth/login') {
+  // Hapus timer lama jika ada
+  if (timer) {
+    clearTimeout(timer)
+  }
+
+  if (localStorage.getItem('token') && route.path !== '/auth/login') {
+    // Set timer untuk menghapus token dalam 5 menit
+    timer = setTimeout(() => {
+      localStorage.removeItem('token')
+      return redirect('/auth/login')
+    }, 1800000) // 60000 ms = 5 menit
+  } else if (!localStorage.getItem('token') && route.path !== '/auth/login') {
     return redirect('/auth/login')
   }
 }
